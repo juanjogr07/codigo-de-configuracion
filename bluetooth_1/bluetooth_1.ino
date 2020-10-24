65a601547bb9ebdc9c9db9b6339f51b93fa869de
juanjogr07 <70668478+juanjogr07@users.noreply.github.com>
2020-10-24 13:21:58 -0500

    Add files via upload

---
+++ bluetooth_1/bluetooth_1.ino
@@ -0,0 +1,37 @@
+const int led =13; 
+const int BTPWR=12;
+
+char nombreBT[11]="techKrowdM";
+char pin[5]="0000";
+char velocidad='4';
+char modo='1';
+
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(BTPWR, OUTPUT);
+  Serial.begin(38400);
+  digitalWrite(led, HIGH);
+  delay(4000);
+   digitalWrite(led, LOW);
+  digitalWrite(BTPWR, HIGH);
+  delay(3000);  
+  Serial.print("AT\r\t");
+  Serial.print("AT+NEME:");
+  Serial.print(nombreBT);
+  Serial.print("\r\t");
+  Serial.print("AT+PIN:");
+  Serial.print(pin);
+  Serial.print("\r\t");
+  Serial.print("AT+BAUD");
+  Serial.print(velocidad);
+   Serial.print("\r\t");
+   Serial.print("AT+MODE:");
+   Serial.print(modo);
+    Serial.print("\r\t");
+    
+    digitalWrite(led, HIGH);
+   
+  }
+  void loop(){
+    
+    }
